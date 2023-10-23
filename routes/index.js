192f418a210b97031ce523ba38c00cbd8883e10e
riponcc22 <mhoss206@my.centennialcollege.ca>
2023-10-22 21:14:18 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+
+//router to send pages to server
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET About Us page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About'});
+});
+
+/* GET Projets page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects'});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services'});
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact'});
+});
+
+module.exports = router;
